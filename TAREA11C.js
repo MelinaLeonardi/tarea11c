c3ced9661e876c3702f06a5b78f78a482c70e8ff
Melina Leonardi <109112626+MelinaLeonardi@users.noreply.github.com>
2022-07-25 20:54:25 -0300

    Add files via upload

---
+++ TAREA11C.js
@@ -0,0 +1,12 @@
+ /* ejercicio 11c >modificamos la extructura base de un "hola mundo" en base al prompt de ingreso del usuario de las veces que quiere que se repita el mensaje*/
+ var inicio;
+ let ningreso = prompt("¿Cuantas veces deseas repetir el mensaje?");
+ 
+ console.log(ningreso);
+ for ( inicio=0; inicio<ningreso;inicio ++)
+
+{
+     document.write("HOLA MUNDO" + "<br>");
+    
+ }
+
